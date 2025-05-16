2fec70fa416d6c2778939f73fd5ce77143d0bb12
0xJihan <jihankhan966@gmail.com>
2025-05-16 16:07:10 +0600

    Removed maxLines from CxSnackBarItem

--- app/src/main/java/com/jihan/computils/MainActivity.kt
+++ app/src/main/java/com/jihan/computils/MainActivity.kt
@@ -6,19 +6,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.jihan.composeutils.core.CenterBox
+import com.jihan.composeutils.core.Gap
 import com.jihan.composeutils.ui.CxButton
+import com.jihan.composeutils.ui.CxSnackBar
+import com.jihan.composeutils.ui.CxSnackBarPosition
+import com.jihan.composeutils.ui.rememberCxSnackBarState
 import com.jihan.computils.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             AppTheme {
-                ComposeUtils()
+                DemoScreen()
 
 
             }
@@ -26,16 +32,41 @@ class MainActivity : ComponentActivity() {
     }
 
 
-@Composable
-fun ComposeUtils() {
-    CenterBox {
+    @Composable
+    fun DemoScreen() {
 
-        CxButton {
+
+
+        val successSnack = rememberCxSnackBarState()
+        val warningSnack = rememberCxSnackBarState()
+        val errorSnack = rememberCxSnackBarState()
+        val customSnack = rememberCxSnackBarState()
+
+
+
+
+        CenterBox {
+
+            CxButton("Success", Modifier.fillMaxWidth()) { successSnack.addMessage("Success SnackBar") }
+            Gap(20)
+            CxButton("Error", Modifier.fillMaxWidth()) { errorSnack.addMessage("Error SnackBar") }
+            Gap(20)
+            CxButton("Warning", Modifier.fillMaxWidth()) { warningSnack.addMessage("Warning SnackBar") }
         }
-
+        CxSnackBar.Success(
+            state = successSnack,
+            position = CxSnackBarPosition.Top
+        )
+        CxSnackBar.Error(
+            state = errorSnack,
+            position = CxSnackBarPosition.Bottom
+        )
+        CxSnackBar.Warning(
+            state = warningSnack,
+            position = CxSnackBarPosition.Float
+        )
 
     }
-}
 
 }
 
--- computils/src/main/java/com/jihan/composeutils/ui/CxSnackBar.kt
+++ computils/src/main/java/com/jihan/composeutils/ui/CxSnackBar.kt
@@ -35,6 +35,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
@@ -45,6 +48,14 @@ import com.jihan.composeutils.R
 import com.jihan.composeutils.core.Cx
 import java.util.Timer
 import kotlin.concurrent.schedule
+
+import androidx.compose.ui.graphics.PathFillType.Companion.EvenOdd
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
 
 
 
@@ -146,7 +157,7 @@ fun CxSnackBar(
 }
 
 @Composable
-fun SnackBarItem(
+private fun SnackBarItem(
     message: String?,
     position: CxSnackBarPosition,
     backgroundColor: Color,
@@ -197,8 +208,6 @@ fun SnackBarItem(
                 modifier = Modifier,
                 text = message ?: "This is a snackbar",
                 style = textStyle,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1
             )
         }
     }
@@ -334,3 +343,151 @@ fun rememberCxSnackBarState() : CxSnackBarState {
     }
 }
 
+
+object SnackbarIcon
+
+// ICON
+
+public val SnackbarIcon.IcSuccess: ImageVector
+    get() {
+        if (_icSuccess != null) {
+            return _icSuccess!!
+        }
+        _icSuccess =
+            Builder(
+                name = "IcSuccess", defaultWidth = 800.0.dp, defaultHeight = 800.0.dp,
+                viewportWidth = 512.0f, viewportHeight = 512.0f,
+            ).apply {
+                path(
+                    fill = SolidColor(Cx.green600), stroke = SolidColor(Cx.green600),
+                    strokeLineWidth = 1.0f, strokeLineCap = Butt, strokeLineJoin = Miter,
+                    strokeLineMiter = 4.0f, pathFillType = EvenOdd,
+                ) {
+                    moveTo(256.0f, 42.667f)
+                    curveTo(138.18f, 42.667f, 42.667f, 138.18f, 42.667f, 256.0f)
+                    curveTo(42.667f, 373.82f, 138.18f, 469.333f, 256.0f, 469.333f)
+                    curveTo(373.82f, 469.333f, 469.333f, 373.82f, 469.333f, 256.0f)
+                    curveTo(469.333f, 138.18f, 373.82f, 42.667f, 256.0f, 42.667f)
+                    close()
+                    moveTo(256.0f, 426.667f)
+                    curveTo(161.895f, 426.667f, 85.333f, 350.105f, 85.333f, 256.0f)
+                    curveTo(85.333f, 161.895f, 161.895f, 85.333f, 256.0f, 85.333f)
+                    curveTo(350.105f, 85.333f, 426.667f, 161.895f, 426.667f, 256.0f)
+                    curveTo(426.667f, 350.105f, 350.106f, 426.667f, 256.0f, 426.667f)
+                    close()
+                    moveTo(336.336f, 179.781f)
+                    lineTo(366.503f, 209.948f)
+                    lineTo(234.667f, 342.336f)
+                    lineTo(155.583f, 263.252f)
+                    lineTo(185.75f, 233.086f)
+                    lineTo(234.667f, 282.003f)
+                    lineTo(336.336f, 179.781f)
+                    close()
+                }
+            }
+                .build()
+        return _icSuccess!!
+    }
+
+private var _icSuccess: ImageVector? = null
+
+
+
+public val SnackbarIcon.IcWarning: ImageVector
+    get() {
+        if (_icWarning != null) {
+            return _icWarning!!
+        }
+        _icWarning =
+            Builder(
+                name = "IcWarning", defaultWidth = 800.0.dp, defaultHeight = 800.0.dp,
+                viewportWidth = 512.0f, viewportHeight = 512.0f,
+            ).apply {
+                path(
+                    fill = SolidColor(Cx.yellow500), stroke = SolidColor(Cx.yellow500),
+                    strokeLineWidth = 1.0f, strokeLineCap = Butt, strokeLineJoin = Miter,
+                    strokeLineMiter = 4.0f, pathFillType = EvenOdd,
+                ) {
+                    moveTo(278.313f, 48.296f)
+                    curveTo(284.928f, 52.075f, 290.41f, 57.557f, 294.189f, 64.172f)
+                    lineTo(476.667f, 383.508f)
+                    curveTo(488.358f, 403.967f, 481.25f, 430.03f, 460.791f, 441.722f)
+                    curveTo(454.344f, 445.405f, 447.047f, 447.343f, 439.622f, 447.343f)
+                    lineTo(74.667f, 447.343f)
+                    curveTo(51.103f, 447.343f, 32.0f, 428.241f, 32.0f, 404.677f)
+                    curveTo(32.0f, 397.251f, 33.938f, 389.955f, 37.622f, 383.508f)
+                    lineTo(220.099f, 64.172f)
+                    curveTo(231.79f, 43.713f, 257.854f, 36.605f, 278.313f, 48.296f)
+                    close()
+                    moveTo(257.144f, 85.341f)
+                    lineTo(74.667f, 404.677f)
+                    lineTo(439.622f, 404.677f)
+                    lineTo(257.144f, 85.341f)
+                    close()
+                    moveTo(256.0f, 314.667f)
+                    curveTo(271.238f, 314.667f, 282.667f, 325.931f, 282.667f, 341.291f)
+                    curveTo(282.667f, 356.651f, 271.238f, 367.915f, 256.0f, 367.915f)
+                    curveTo(240.416f, 367.915f, 229.333f, 356.651f, 229.333f, 340.949f)
+                    curveTo(229.333f, 325.931f, 240.762f, 314.667f, 256.0f, 314.667f)
+                    close()
+                    moveTo(277.333f, 149.333f)
+                    lineTo(277.333f, 277.333f)
+                    lineTo(234.667f, 277.333f)
+                    lineTo(234.667f, 149.333f)
+                    lineTo(277.333f, 149.333f)
+                    close()
+                }
+            }
+                .build()
+        return _icWarning!!
+    }
+
+private var _icWarning: ImageVector? = null
+
+
+public val SnackbarIcon.IcError: ImageVector
+    get() {
+        if (_icError != null) {
+            return _icError!!
+        }
+        _icError =
+            Builder(
+                name = "IcError", defaultWidth = 800.0.dp, defaultHeight = 800.0.dp,
+                viewportWidth = 512.0f, viewportHeight = 512.0f,
+            ).apply {
+                path(
+                    fill = SolidColor(Cx.red600), stroke = SolidColor(Cx.red600),
+                    strokeLineWidth = 1.0f, strokeLineCap = Butt, strokeLineJoin = Miter,
+                    strokeLineMiter = 4.0f, pathFillType = EvenOdd,
+                ) {
+                    moveTo(256.0f, 42.667f)
+                    curveTo(373.61f, 42.667f, 469.333f, 138.39f, 469.333f, 256.0f)
+                    curveTo(469.333f, 373.61f, 373.61f, 469.333f, 256.0f, 469.333f)
+                    curveTo(138.39f, 469.333f, 42.667f, 373.61f, 42.667f, 256.0f)
+                    curveTo(42.667f, 138.39f, 138.39f, 42.667f, 256.0f, 42.667f)
+                    close()
+                    moveTo(256.0f, 85.333f)
+                    curveTo(161.541f, 85.333f, 85.333f, 161.541f, 85.333f, 256.0f)
+                    curveTo(85.333f, 350.459f, 161.541f, 426.667f, 256.0f, 426.667f)
+                    curveTo(350.459f, 426.667f, 426.667f, 350.459f, 426.667f, 256.0f)
+                    curveTo(426.667f, 161.541f, 350.459f, 85.333f, 256.0f, 85.333f)
+                    close()
+                    moveTo(256.0f, 314.709f)
+                    curveTo(271.238f, 314.709f, 282.667f, 325.973f, 282.667f, 341.333f)
+                    curveTo(282.667f, 356.693f, 271.238f, 367.957f, 256.0f, 367.957f)
+                    curveTo(240.416f, 367.957f, 229.333f, 356.693f, 229.333f, 340.992f)
+                    curveTo(229.333f, 325.973f, 240.762f, 314.709f, 256.0f, 314.709f)
+                    close()
+                    moveTo(277.333f, 128.0f)
+                    lineTo(277.333f, 277.333f)
+                    lineTo(234.667f, 277.333f)
+                    lineTo(234.667f, 128.0f)
+                    lineTo(277.333f, 128.0f)
+                    close()
+                }
+            }
+                .build()
+        return _icError!!
+    }
+
+private var _icError: ImageVector? = null
